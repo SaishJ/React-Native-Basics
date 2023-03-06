6443b33108200962657fc71624382bdb54b8ba74
SaishJ <saishjagtap0003@gmail.com>
2023-03-06 17:29:33 +0530

    :clown_face: Image
    Image

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, SafeAreaView, Text } from "react-native";
+import { StyleSheet, View, SafeAreaView, Text, Image } from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("Text click");
@@ -9,6 +9,11 @@ export default function App() {
         Hello React Native - A framework for building native apps using
         JavaScript.
       </Text>
+      <Image source={require("./assets/react.png")} />
+      <Image
+        fadeDuration={1000}
+        source={{ width: 200, height: 200, uri: "https://picsum.photos/200" }}
+      />
     </SafeAreaView>
   );
 }
@@ -16,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "dodgerblue",
+    backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
   },
--- Readme.md
+++ Readme.md
@@ -13,6 +13,12 @@
   > SafeAreaView renders nested content and automatically applies padding to reflect the portion of the view that is not covered by navigation bars, tab bars, toolbars, and other ancestor views.
 
 - Text
+
   > Used for displaying text. `<Text>` support nesting, styling and touch handling. Text also access props like, numberOfLines, onPress, etc.
   > `numberOfLines`: use for ellipsize.
   > `onPress` : make text clickable.
+
+- Image
+  > In React Native displaying different types of images, including network images, static resources, temporary local images, and images from local disk, such as the camera roll.
+  > In local images use `require` to load images.
+  > In network images use `uri` to load image and also set `width` and `height` props.
